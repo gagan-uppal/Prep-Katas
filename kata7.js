4e280ebd16c185ec74850b2fc450bbff36df616d
Gagandeep Uppal <guppal.profile@gmail.com>
2021-12-20 21:45:57 -0500

    kata 7

---
+++ kata7.js
@@ -0,0 +1,30 @@
+const checkAir = function (samples, threshold) {
+  // Code here!
+  let dirtySample = 0;
+  for (let i =0; i < samples.length; i++){
+    if (samples[i] === "dirty"){
+      dirtySample ++;
+        } 
+      }
+        if((dirtySample / samples.length) > threshold){
+          return "Polluted"
+        } else if ((dirtySample / samples.length) < threshold){
+          return "Clean"
+      
+  }
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
