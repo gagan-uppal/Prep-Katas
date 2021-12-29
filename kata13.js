6b755d91a55083c71553d2cd1cd4a5d8e6257de5
Gagandeep Uppal <guppal.profile@gmail.com>
2021-12-29 14:57:05 -0500

    kata 13

---
+++ kata13.js
@@ -0,0 +1,78 @@
+let talkingCalendar = function(date) {
+  let userDate ;
+  let datearray = date.split("/");
+  let day = datearray[2];
+  let month = datearray[1];
+  let year = datearray[0];
+  let suffix;
+
+  switch(month){
+    case "01":
+      month = "January";
+      break;
+    case "02":
+      month = "February" ;
+      break;
+
+    case "03":
+      month = "March";
+      break;
+
+    case "04":
+      month = "April";
+      break;
+
+    case "05":
+      month = "May";
+      break;
+
+    case "06":
+      month = "June";
+      break;
+
+    case "07":
+      month = "July";
+      break;
+
+    case "08":
+      month = "August";
+      break;
+
+    case "09":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+    case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
+}
+
+switch(day){
+  case "01": suffix = "st";
+  break;
+  case "02": suffix = "nd";
+  break;
+  case '03': suffix = "rd";
+  break;
+  case "21": suffix = "st";
+  break;
+  case "22": suffix = "nd";
+  break;
+  case "23": suffix = "rd";
+  break;
+  default : suffix = "th";
+  break;
+}
+
+userDate = month + " "+ day+suffix+ ", "+ year;
+return userDate;
+}
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"))
