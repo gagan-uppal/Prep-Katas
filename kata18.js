1af3e9ecc3f420d10ae943a4a708e6dd51adc7d1
Gagandeep Uppal <guppal.profile@gmail.com>
2022-01-02 18:23:30 -0500

    kata 18

---
+++ kata18.js
@@ -0,0 +1,16 @@
+const squareCode = function(message) {
+  let message1 = message.split(' ').join('');
+//console.log(message1);
+let message1Length = message1.length;
+//console.log(message1Length);
+let squareRoot = Math.ceil(Math.sqrt(message1Length));
+//console.log(squareRoot);
+for(){
+  
+}
+}
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
